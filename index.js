e75dbd8258efe60ecc6ad9fbd6b9c1f4efdc4832
EZEGreenEnt <ezegreenenterprises@gmail.com>
2024-07-04 15:17:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // index.js
 
-// Get the navigation menu and the hamburger icon
-const navMenu = document.querySelector('nav ul');
-const hamburgerIcon = document.querySelector('.hamburger-icon');
-
 // Toggle the navigation menu when the hamburger icon is clicked
-hamburgerIcon.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
-});
+window.onload = function() {
+  const hamburgerIcon = document.querySelector('.hamburger-icon');
+  const navMenu = document.querySelector('nav ul');
+
+  hamburgerIcon.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+  });
+};
+
